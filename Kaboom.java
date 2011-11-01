c43e377fbc782f82046f13bcae17dbc60b73183c
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-10-31 21:55:37 -0700

    I kinda forgot how to do constructors

--- Kaboom.java
+++ Kaboom.java
@@ -413,7 +413,7 @@ class Pair<E1, E2> {
     public E1 first;
     public E2 second;
     
-    public void Pair<E1, E2>(E1 first, E2 second)
+    public Pair(E1 first, E2 second)
     {
         this.first = first;
         this.second = second;
