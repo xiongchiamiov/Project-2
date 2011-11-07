a71531380d80c9b327f119ef86e8136027e21301
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-11-06 21:16:02 -0800

    unstub GridImages

--- GridImages.java
+++ GridImages.java
@@ -1,3 +1,5 @@
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 
 /**
  * GridImages represents the images to be used for pieces and backgrounds in
@@ -12,6 +14,8 @@ public final class GridImages
     /** The name of the directory containing the images */
     public final static String kImageDir = "PieceImages";
     
+    private static GridImages instance;
+    
     /** Initialize and return the one allowed instance of this class.
      * The instance contains images loaded from <code>kImageDir</code>, one for 
      * each Piece enum value and one named "bkgd".
@@ -20,30 +24,74 @@ public final class GridImages
      * @return an instance of this class. 
      */
     public static GridImages createInstance(String gamePrefix)
+    {
+        if (GridImages.instance == null)
+        {
+            GridImages.instance = new GridImages(gamePrefix);
+        }
+        
+        return GridImages.instance;
+    }
     
     /** Retrieve an image for a piece.
      * @param name the name of the desired image (usually the <code>name()</code> of a piece enum)
      * @return ImageIcon the image associated with the specified name
      */
     public ImageIcon getPiece(String name)
+    {
+        String filename = "";
+        Piece piece = Piece.valueOf(name);
+        switch (piece) {
+            case bomb:
+                filename = "bomb.jpg";
+                break;
+            case empty:
+                filename = "empty.jpg";
+                break;
+            case exploded:
+                filename = "exploded.jpg";
+                break;
+            case flagged:
+                filename = "flagged.jpg";
+                break;
+            case hidden:
+                filename = "hidden.jpg";
+                break;
+        }
+        
+        return new ImageIcon(Toolkit.getDefaultToolkit().getImage(Kaboom.class.getResource(this.kImageDir + "/" + filename)));
+    }
 
     /** Retrieve a background image.
      * @param name the name of the desired image (usually corresponds to a background)
      * @return ImageIcon the image associated with the background
      */
     public ImageIcon getBkgd(String name)
+    {
+        return new ImageIcon(Toolkit.getDefaultToolkit().getImage(Kaboom.class.getResource(this.kImageDir + "/bkgd.jpg")));
+    }
 
     /** Accessor to the piece height. 
      * @return the piece height in pixels.
      */
     public int getPieceHeight() 
+    {
+		// TODO: Non-magify.
+		return 78;
+    }
 
     /** Accessor to the piece width.
      * @return the piece width in pixels.
      */
     public int getPieceWidth() 
+    {
+		// TODO: Non-magify.
+		return 58;
+    }
 
     /** Constructor is private to defeat instantiation */
     private GridImages(String gamePrefix)
+    {
+    }
    
 }
